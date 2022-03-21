e0fe8b4e38600d4fe638183aa8ad0b75ccdfdbdf
Octavio Olea <oleaoctavio23@gmail.com>
2022-03-21 17:48:58 -0600

    Pokedex conectada con fetch poke api para que muestre datos de un pokemon aleatoriamente son 898 posibles resultados.

--- semana-cuatro/assets/js/pokedex.js
+++ semana-cuatro/assets/js/pokedex.js
@@ -1,51 +1,46 @@
-let pokedex = document.querySelector('#pokedex');
-let xInic, yInic;
-let estaPulsado = false;
-/* Mover la pokedex */
+let screenWhite = document.querySelector('.screen-white');
+let screenSecondaryLeft = document.querySelector('.info-final .left');
+let screenSecondaryRight = document.querySelector('.info-final .right');
+let infoMain = document.querySelector('.info-main');
+let id = Math.random()*898;
+let idAleatorio = parseInt(id);
+const url = `https://pokeapi.co/api/v2/pokemon/${idAleatorio}`;
 
-if(pokedex.addEventListener){
-    pokedex.addEventListener("mousedown", pokedown,false);
-    pokedex.addEventListener("mousemove", mousemove,false);
-    pokedex.addEventListener("mouseup", mouseup,false);
-    
-}
+window.addEventListener("load",mostrarPokemon);
 
-function pokedown(e) {
-    xInic = e.clientX;
-    yInic = e.clientY;    
-    estaPulsado = true;
-}
+function mostrarPokemon(){
+    let hola ="fdsf";
+    console.log(hola.toUpperCase());
+    fetch(url).then(res=>res.json()).then(data=>{console.log(data);
+        console.log(`imagen: ${data.sprites.front_default}`);
+        const html=`<img src="${data.sprites.front_default}" alt="${data.name}"
+        class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 w-100 h-75 p-0">
+        <p class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center p-0"><b>#${data.id} ${data.name.toUpperCase()}</b></p>`;
+        screenWhite.innerHTML=html;
 
-function mousemove(e) {
-    if(estaPulsado) {
-        //Calcular la diferencia de posición
-        let xActual = e.clientX;
-        let yActual = e.clientY;    
-        let xInc = xActual-xInic;
-        let yInc = yActual-yInic;
-        xInic = xActual;
-        yInic = yActual;
+        const htmlHeight=`<p class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center p-0 text-warning"><b>ALTURA ${data.height} M</b></p>`;
+        screenSecondaryLeft.innerHTML = htmlHeight;
+
+        const htmlWeight=`<p class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center p-0 text-warning"><b>PESO ${data.weight} KG</b></p>`;
+        screenSecondaryRight.innerHTML = htmlWeight;
         
-        //Establecer la nueva posición
-        let position = getPosicion(pokedex);
-        pokedex.style.top = (position[0] + yInc) + "px";
-        pokedex.style.left = (position[1] + xInc) + "px";
-    }
-}
+        let main =[];
+        let mainUNO="";
+        let mainDOS="";
 
-function mouseup() {
-    estaPulsado = false;
-}
+        for(let i=0;i<data.types.length;i++){
+            const htmlMain = `<small class="letra col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center text-warning"><b>${data.types[i].type.name.toUpperCase()}</b></small>`;
+            mainUNO=htmlMain;
+            main.push(mainUNO);
+        }
 
-function getPosicion(elemento) {
-    let posicion = new Array(2);
-    if(document.defaultView && document.defaultView.getComputedStyle) {
-        posicion[0] = parseInt(document.defaultView.getComputedStyle(pokedex, null).getPropertyValue("top"))
-        posicion[1] = parseInt(document.defaultView.getComputedStyle(pokedex, null).getPropertyValue("left"));
-    } else {
-        //Para Internet Explorer
-        posicion[0] = parseInt(pokedex.currentStyle.top);             
-        posicion[1] = parseInt(pokedex.currentStyle.left);               
-    }      
-    return posicion;
+        for(let i=0;i<data.stats.length;i++){
+            const htmlMain = `<small class="letra col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center text-warning"><b>${data.stats[i].stat.name.toUpperCase()} ${data.stats[i].base_stat}</b></small>`;
+            mainDOS=htmlMain;
+            main.push(mainDOS);
+        }
+        console.log(main)
+        infoMain.innerHTML =  main;
+
+});
 }
